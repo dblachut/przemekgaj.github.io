703438765c4c3348b94d25920955fbb53cd12886
przemekgaj <prz.gaj@gmail.com>
2014-06-18 22:49:16 +0200

    add new function feature

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 		
 		var addFunction = $('.functions-listview').find('li:last').clone();
 		addFunction.addClass('ui-first-child')
-		addFunction.find('a').removeClass('ui-icon-carat-r').addClass('ui-icon-plus').addClass('add-function').html('Dodaj funkcję');
+		addFunction.find('a').removeClass('ui-icon-carat-r').addClass('ui-icon-plus').addClass('add-function').html('Dodaj funkcję').attr('href', '#add-function');
 		$('.functions-listview').append(addFunction);
 		
 		$('.functions-listview').find('a').each(function(){

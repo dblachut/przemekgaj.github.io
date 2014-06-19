b1206f4ed14ee50615501ac1419b42f7840d1183
przemekgaj <prz.gaj@gmail.com>
2014-06-19 16:18:51 +0200

    local SQL fix 3

--- js/script.js
+++ js/script.js
@@ -102,8 +102,8 @@ $(document).ready(function(){
 	$(document).on('tap','.edit-functions',  function(){
 		
 		var addFunction = $('.functions-listview').find('li:last').clone();
-		addFunction.addClass('ui-first-child')
-		addFunction.find('a').removeClass('ui-icon-carat-r').removeClass('ui-invisible').addClass('ui-icon-plus').addClass('add-function').html('Dodaj funkcję').attr('href', '#add-function');
+		addFunction.addClass('ui-first-child').removeClass('ui-invisible');
+		addFunction.find('a').removeClass('ui-icon-carat-r').addClass('ui-icon-plus').addClass('add-function').html('Dodaj funkcję').attr('href', '#add-function');
 		$('.functions-listview').append(addFunction);
 		
 		$('.functions-listview').find('a').each(function(){

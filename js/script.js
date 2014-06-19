a9c813649d0dfc78d6d47a9152a4a107aef9f3eb
przemekgaj <prz.gaj@gmail.com>
2014-06-19 16:12:17 +0200

    local SQL db connected + function add

--- js/script.js
+++ js/script.js
@@ -8,7 +8,96 @@ function checkFormula(formula){
 	
 }
 
+var db;
+
+function createDb(){
+	db = openDatabase ("Test", "1.0", "Test", 65535);
+	
+	db.transaction (function (transaction) {
+		var sql = "CREATE TABLE functions " +
+				  " (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
+		          "name VARCHAR(100) NOT NULL, " + 
+		          "func TEXT)";
+		transaction.executeSql (sql, undefined, function (){ 
+			console.log ("Table created");
+		},function () { 
+			console.log ("Table not created");
+		});
+	});
+}
+
+function dropDb(){
+	db = openDatabase ("Test", "1.0", "Test", 65535);
+
+	db.transaction (function (transaction) {
+		var sql = "DROP TABLE functions";
+		transaction.executeSql (sql, undefined, function (){ 
+			console.log ("Table deleted");
+		},function () { 
+			console.log ("Table not deleted");
+		});
+	});
+}
+
+function insertFunctionToDb(name, func){
+	db = openDatabase ("Test", "1.0", "Test", 65535);
+	
+	var ret = false;
+	db.transaction (function (transaction) {
+	    var sql = "INSERT INTO functions (name, func) VALUES (?, ?)";
+	    transaction.executeSql (sql, [name, func], function (){ 
+	      	ret = true;
+	    }, function (){ 
+	      	ret = false;
+	    });
+	});
+	return ret;
+}
+
+function selectFromDb(){
+	db = openDatabase ("Test", "1.0", "Test", 65535);
+	$('.functions-listview li').remove();
+	$('.appended-functions').remove();
+	
+	db.transaction (function (transaction) {
+	    var sql = "SELECT * FROM functions";
+	    transaction.executeSql (sql, undefined, 
+	    function (transaction, result){
+	      if (result.rows.length){
+	        for (var i = 0; i < result.rows.length; i++){
+	          var row = result.rows.item (i);
+	          var id = row.id;
+	          var name = row.name;
+	          var func = row.func;
+	          
+	          $('.functions-listview').append('<li><a href="#f' + id + '" data-transition="slide" class="ui-btn ui-btn-icon-right ui-icon-carat-r">' + name + '</a></li>');
+	          
+	          var page = $('#function-sketch').clone();
+	          page.attr('id', 'f' + id);
+	          page.addClass('appended-functions');
+	          page.find('h1').html(name);
+	          
+	          //$('div[data-role="page"]').append(page);
+	          page.appendTo('body');
+	          //console.log(page);
+	        }
+	      }
+	      else {
+	      	
+	      }
+	      
+	      $('.functions-listview li').removeClass('ui-last-child');
+	      $('.functions-listview li:last').addClass('ui-last-child');
+	      
+	    }, function(){
+		    
+	    });
+	});
+}
+
 $(document).ready(function(){
+	createDb();
+	selectFromDb();
 	
 	$(document).on('tap','.edit-functions',  function(){
 		
@@ -39,15 +128,22 @@ $(document).ready(function(){
 		
 	});
 	
-	/*$(document).on('tap', '.check-formula', function(){
+	$(document).on('tap', '.save-formula', function(){
+		//console.log($('input[name="formula-name"]').val());
+		//console.log($('input[name="formula"]').text());
+		insertFunctionToDb($('input[name="formula-name"]').val(), $('textarea[name="formula"]').val());
+		selectFromDb();
+		$('#add-function').removeClass('ui-page-active');
+		$.mobile.changePage('#main', {transition: "slide"});
+	});
+	
+	$(document).on('tap', '.resolv-function', function(){
 		
-		if(checkFormula($('input[name="formula"]').text())){
-			$('#overlay').show();
-		}
-		else {
-			$('#overlay').show();
-		}
 		
-	});*/
+		//dropDb();
+		//createDb();
+		//return false;
+		
+	});
 	
 });

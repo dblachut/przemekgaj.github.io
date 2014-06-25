e98d6af536b62977dce519274dee664128271e5c
przemekgaj <prz.gaj@gmail.com>
2014-06-25 10:53:35 +0200

    full screen 4

--- js/script.js
+++ js/script.js
@@ -8,12 +8,12 @@ $(document).ready(function(){
 	
 if (document.body.webkitRequestFullScreen) {
   window.addEventListener('click', function(e) {
-    if (e.target.type != 'text' && e.target.type != 'password') {
+    //if (e.target.type != 'text' && e.target.type != 'password') {
       body.webkitRequestFullScreen();
       window.setTimeout(function() {
         document.webkitCancelFullScreen();
       }, 500);
-    }
+    //}
   }, false);
 }
 	//dropDb();

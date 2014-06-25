dd0ed2fbf66dc1ab384ccfbbdbf96cf9009cf3cd
przemekgaj <prz.gaj@gmail.com>
2014-06-25 10:48:56 +0200

    full screen

--- js/script.js
+++ js/script.js
@@ -3,7 +3,24 @@ var charts = [];
 var chartWidth;
 var chartHeight;
 
+function toggleFullScreen() {
+  var doc = window.document;
+  var docEl = doc.documentElement;
+
+  var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+  var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+  if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+    requestFullScreen.call(docEl);
+  }
+  else {
+    cancelFullScreen.call(doc);
+  }
+}
+
 $(document).ready(function(){
+
+	toggleFullScreen();
 
 	//dropDb();
 	connectDb();

7775edfdc4cd629cec0caaba5536f49d86d14c3d
przemekgaj <prz.gaj@gmail.com>
2014-10-25 17:43:04 +0200

    js update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
 var debug = true;
+var server;
 
 $(document).ready(function(){
 	
 	if(debug) console.log('Starting...');
 	
+	server = $('select[name="server"]').val();
 	
+	$('form.signin').submit(function(e){
+		e.preventDefault();
+		
+		var array = {
+			login: $('input[name="nickname"]')
+		}
+		
+		$.post(server, array, function(data){
+			if(debug) console.log(data);
+			
+			
+		});
+		
+	});
 	
 	
 });
